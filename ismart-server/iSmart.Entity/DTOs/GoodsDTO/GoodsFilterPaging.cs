5df87455e96693838329120a109643ca55165293
Alan_Do_2102 <thanhdthe150750@gmail.com>
2024-05-22 15:12:55 +0700

    Create GoodsDTO

---
+++ ismart-server/iSmart.Entity/DTOs/GoodsDTO/GoodsFilterPaging.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WM.Entity.Models;
+
+namespace WM.Entity.DTOs.GoodsDTO
+{
+    public class GoodsFilterPaging : ResponseData<GoodsDTO>
+    {
+
+    }
+}
+
+
+
