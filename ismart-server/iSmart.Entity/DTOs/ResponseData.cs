f780e68af6f1c1258d3c2c0ca2580e1c05b04327
Alan_Do_2102 <thanhdthe150750@gmail.com>
2024-05-26 17:00:12 +0700

    Add DeliveryDTO

---
+++ ismart-server/iSmart.Entity/DTOs/ResponseData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WM.Entity.Models;
+namespace WM.Entity.DTOs
+{
+    public class ResponseData<T>
+    {
+        public List<T> Data { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
